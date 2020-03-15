7fb1d8fbd2b848e670eafdadc2776d424fb5be22
Ilya Kuliashou <proxinadu@gmail.com>
2020-03-15 16:20:31 +0300

    Added texture class.

---
+++ GameEngineLibrary/Texture2D.cs
@@ -0,0 +1,16 @@
+﻿namespace GameEngineLibrary
+{
+    public class Texture2D
+    {
+        public int ID { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public Texture2D(int id, int width, int height)
+        {
+            ID = id;
+            Width = width;
+            Height = height;
+        }
+    }
+}
