ce5615391db297dbbb8ab3af4b667fb5012398c4
Ilya Kuliashou <proxinadu@gmail.com>
2020-03-15 17:05:55 +0300

    Added GameObject class.

---
+++ GameEngineLibrary/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK;
+
+namespace GameEngineLibrary
+{
+    /// <summary>
+    /// Класс, который описывает объект на сцене.
+    /// </summary>
+    public class GameObject
+    {
+        /// <summary>
+        /// Текстура объекта.
+        /// </summary>
+        public Texture2D Texture { get; private set; }
+        /// <summary>
+        /// Точка, вокруг которой будет поворачиваться объект.
+        /// </summary>
+        public Vector2 RotationPoint { get; private set; }
+        /// <summary>
+        /// Позиция объекта.
+        /// </summary>
+        public Vector2 Position { get; set; }
+        /// <summary>
+        /// Масштабирование объект.
+        /// </summary>
+        public Vector2 Scale { get; set; }
+        /// <summary>
+        /// Угол поворота объекта.
+        /// </summary>
+        public double Rotation { get; set; }
+    }
+}
