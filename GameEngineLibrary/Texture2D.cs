0de060cdc7494dde1619ecedbf0c5f5007a19cd4
Ilya Kuliashou <proxinadu@gmail.com>
2020-03-15 17:31:59 +0300

    Added method for loading textures from file.

--- GameEngineLibrary/Texture2D.cs
+++ GameEngineLibrary/Texture2D.cs
@@ -1,4 +1,8 @@
-﻿namespace GameEngineLibrary
+﻿using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace GameEngineLibrary
 {
     /// <summary>
     /// Класс, который описывает двухмерную текстуру.
@@ -29,6 +33,46 @@
             ID = id;
             Width = width;
             Height = height;
+        }
+
+        /// <summary>
+        /// Загрузка текстуры из файла.
+        /// </summary>
+        /// <param name="path">Путь к файлу.</param>
+        /// <returns>Загруженная текстура.</returns>
+        public static Texture2D LoadTexture(string path)
+        {
+            int id = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, id);
+
+            Bitmap bitmap = new Bitmap(path);
+            BitmapData data = bitmap.LockBits(
+                new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0,
+                PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                PixelType.UnsignedByte, data.Scan0);
+
+            bitmap.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D,
+                TextureParameterName.TextureWrapS,
+                (int)TextureWrapMode.Clamp);
+            GL.TexParameter(TextureTarget.Texture2D,
+                TextureParameterName.TextureWrapT,
+                (int)TextureWrapMode.Clamp);
+
+            GL.TexParameter(TextureTarget.Texture2D,
+                TextureParameterName.TextureMinFilter,
+                (int)TextureMinFilter.Nearest);
+            GL.TexParameter(TextureTarget.Texture2D,
+                TextureParameterName.TextureMagFilter,
+                (int)TextureMagFilter.Nearest);
+
+            return new Texture2D(id, bitmap.Width, bitmap.Height);
         }
     }
 }
