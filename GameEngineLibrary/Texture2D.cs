a1feb477b5c19420123b5b08561969ad0afe61c8
Ilya Kuliashou <proxinadu@gmail.com>
2020-03-15 16:25:05 +0300

    Added comments.

--- GameEngineLibrary/Texture2D.cs
+++ GameEngineLibrary/Texture2D.cs
@@ -1,11 +1,29 @@
 ﻿namespace GameEngineLibrary
 {
+    /// <summary>
+    /// Класс, который описывает двухмерную текстуру.
+    /// </summary>
     public class Texture2D
     {
+        /// <summary>
+        /// Идентификатор текстуры.
+        /// </summary>
         public int ID { get; private set; }
+        /// <summary>
+        /// Ширина текстуры.
+        /// </summary>
         public int Width { get; private set; }
+        /// <summary>
+        /// Высота текстуры.
+        /// </summary>
         public int Height { get; private set; }
 
+        /// <summary>
+        /// Создание новой текстуры.
+        /// </summary>
+        /// <param name="id">Идентификатор текстуры.</param>
+        /// <param name="width">Ширина текстуры.</param>
+        /// <param name="height">Высота текстуры.</param>
         public Texture2D(int id, int width, int height)
         {
             ID = id;
