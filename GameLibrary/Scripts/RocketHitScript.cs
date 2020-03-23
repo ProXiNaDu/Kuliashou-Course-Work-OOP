19bf1eaff875000e38cf703ae5f724350266296f
Ilya Kuliashou <proxinadu@gmail.com>
2020-03-24 00:37:19 +0300

    Added rocket hit script.

---
+++ GameLibrary/Scripts/RocketHitScript.cs
@@ -0,0 +1,37 @@
+﻿using GameEngineLibrary;
+using System;
+
+namespace GameLibrary.Scripts
+{
+    /// <summary>
+    /// Класс, отвечающий за проверку поведение 
+    /// при попадании ракеты по цели.
+    /// </summary>
+    public class RocketHitScript : Script
+    {
+        private IScene scene;
+
+        /// <summary>
+        /// Создание скрипта, отвечающего за обрапотку попаданий ракеты.
+        /// </summary>
+        /// <param name="scene">Сцена, в которой будет проверяться столкновения.</param>
+        public RocketHitScript(IScene scene)
+        {
+            this.scene = scene;
+        }
+
+        public override void Update(TimeSpan delta)
+        {
+            GameObject[] objects = scene.GetGameObjects().ToArray();
+            foreach (GameObject gameObject in objects)
+            {
+                if (gameObject != controlledObject &&
+                    gameObject.Collider != null &&
+                    gameObject.Collider.CheckCollision(controlledObject.Collider))
+                {
+                    //
+                }
+            }
+        }
+    }
+}
